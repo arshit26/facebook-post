1e96cebe21cc0f93522ae9ac1de1c54b21ee9f0d
arshit26 <arshitaggarwal26@gmail.com>
2017-06-20 22:02:17 +0530

    Added Color Select Code

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	$(document).ready(function(){
 		// Creating an Ajax Call on Click of Submit Button
 		$("#btn_submit").click(function(){
-			$.get("post_image.php?quote="+$("#quote").val(), function(data){
+			$.get("post_image.php?quote="+$("#quote").val()+"&back_col="+encodeURIComponent($("#background_color").val())+"&text_col="+encodeURIComponent($("#text_color").val()), function(data){
 				$("#result").text(data.message);	// It will show the result in an alert
 			});
 		});
@@ -17,16 +17,26 @@
 </head>
 <body bgcolor="#1A237E">
 	<div class="w3-container">
-	<center>
-		<div class="w3-container w3-display-middle w3-round w3-white w3-card-4" style="padding:0">
+			<div class="w3-container w3-display-middle w3-round w3-white w3-card-4" style="padding:0">
 		<div class="w3-row w3-card-4 w3-padding"><h2 style="color:#1A237E">Give a Quote to post on Facebook as an Image</h2></div>
 		<br><br>
 		<div class="w3-row w3-padding">
 				<input type="text" id="quote" placeholder="Give a Quote" class="w3-input" autofocus="true" maxlength=250 required/><br>
-				<input type="submit" id="btn_submit" class="w3-button w3-grey w3-hover-green w3-round"/>
+		</div>
+		<div class="w3-row w3-padding">
+				<span class="w3-text" style="color:#1A237E" align=left><b>Select Background Color</b></span>
+				<input style="float:right" type="color" id="background_color" value="#ff0000"/><br>
+		</div>
+		<div class="w3-row w3-padding">
+				<span  class="w3-text" style="color:#1A237E"><b>Select Text Color</b></span>
+				<input style="float:right" class=""type="color" id="text_color" value="#ffff00"/><br>
+		</div>
+		<div class="w3-row w3-padding">
+				<center><input style="" type="submit" id="btn_submit" class="w3-button w3-grey w3-hover-green w3-round"/></center>
 		</div>
 		</div>
-		<h3 style="color:#FFFFFF" id="result"></h3> <!-- The result will be shown here -->
+		<center>
+		<h3 style="color:#FFFFFF" id="result"></h3>
 				</center>
 	</div>
 </body>
